1aec357e14d76d53fa3bb08be43e6def5df577a4
Facu Pacheco <facundomartinpacheco26@gmail.com>
2025-06-11 18:43:22 -0300

    Implementé la función home(request) en views.py

--- app/views.py
+++ app/views.py
@@ -4,16 +4,20 @@ from django.shortcuts import redirect, render
 from .layers.services import services
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
+from layers.services import services
 
 def index_page(request):
     return render(request, 'index.html')
 
 # esta función obtiene 2 listados: uno de las imágenes de la API y otro de favoritos, ambos en formato Card, y los dibuja en el template 'home.html'.
 def home(request):
-    images = []
+    images = services.getAllImages()
     favourite_list = []
 
-    return render(request, 'home.html', { 'images': images, 'favourite_list': favourite_list })
+    return render(request, 'home.html', {
+        'images': images,
+        'favourite_list': favourite_list
+    })
 
 # función utilizada en el buscador.
 def search(request):
