41b2daca8e13f1b0b0461af6020f5853058e3f34
Santiago Verón <veron.santiago@outlook.com>
2025-06-27 16:18:49 -0300

    Rediseño de las cards

--- app/layers/utilities/translator.py
+++ app/layers/utilities/translator.py
@@ -7,7 +7,7 @@ def fromRequestIntoCard(poke_data):
     card = Card(
         id=poke_data.get('id'),
         name=poke_data.get('name').capitalize(),
-        height=str(float(poke_data.get('height')) / 10) + " M",
+        height=str(float(poke_data.get('height')) / 10) + " Mts",
         weight=str(float(poke_data.get('weight')) / 10) + " Kg",
         base=poke_data.get('base_experience'),
         image=safe_get(poke_data, 'sprites', 'other', 'official-artwork', 'front_default'),
